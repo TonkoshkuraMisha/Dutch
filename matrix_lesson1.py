9ce775dd89785e36519578ce3751b5dd71e6ccca
tonkoshkura.misha <tonkoshkura.misha@gmail.com>
2022-09-24 18:04:44 +0300

    changed main files

--- matrix_lesson1.py
+++ matrix_lesson1.py
@@ -1,20 +1,17 @@
-# This function add matrix
-def matrix_add(mx1: list, mx2: list):
-    if len(mx1) == len(mx2):
-        n = len(mx1[0])
-        matrix = [[0] * m1]* n
-        for i in range(m1):
-            for j in range(n):
-                matrix[i][j] = matrix1[i][j] + matrix2[i][j]
-    else:
-        print("You must add equal lens matrix")
-    return matrix
+from out_matrix import *
 
+m = int(input())
+result = [[0] * m] * m
 
-m1 = int(input())
-matrix1 = [list(map(int, input().split())) for _ in range(m1)]
-m2 = m1
-matrix2 = [list(map(int, input().split())) for _ in range(m2)]
+matrix1 = [list(map(int, input().split())) for _ in range(m)]
+matrix2 = [list(map(int, input().split())) for _ in range(m)]
 
-result = matrix_add(matrix1, matrix2)
-print(result)
+print_matrix(result, m)
+print_matrix(matrix1, m)
+print_matrix(matrix2, m)
+
+for i in range(m):
+    for j in range(m):
+        result[i][j] = matrix1[i][j] + matrix2[i][j]
+
+print_matrix(result, m)
---
+++ out_matrix.py
@@ -0,0 +1,5 @@
+def print_matrix(matrix, n, width=1):
+    for r in range(n):
+        for c in range(n):
+            print(str(matrix[r][c]).ljust(width), end=' ')
+        print()
