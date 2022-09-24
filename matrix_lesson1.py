6fce36bd10da1e327941e96b9550b8abcda5b8d7
Onixix123 <bogdan.iakovliev@gmail.com>
2022-09-24 17:12:50 +0300

    some text

--- matrix_lesson1.py
+++ matrix_lesson1.py
@@ -1,11 +1,20 @@
-# git terminal
-# power shell
-# command line
+# This function add matrix
+def matrix_add(mx1: list, mx2: list):
+    if len(mx1) == len(mx2):
+        n = len(mx1[0])
+        matrix = [[0] * m1]* n
+        for i in range(m1):
+            for j in range(n):
+                matrix[i][j] = matrix1[i][j] + matrix2[i][j]
+    else:
+        print("You must add equal lens matrix")
+    return matrix
 
 
-# n1 = int(input())
 m1 = int(input())
 matrix1 = [list(map(int, input().split())) for _ in range(m1)]
-print(matrix1)
+m2 = m1
+matrix2 = [list(map(int, input().split())) for _ in range(m2)]
 
-# Bogdan, Hi!
+result = matrix_add(matrix1, matrix2)
+print(result)
