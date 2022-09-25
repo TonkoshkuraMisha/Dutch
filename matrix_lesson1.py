1c6bf538776af577dad2944282078f12aadbdea5
tonkoshkura.misha <tonkoshkura.misha@gmail.com>
2022-09-25 10:51:22 +0300

    matrix bug fix

--- matrix_lesson1.py
+++ matrix_lesson1.py
@@ -1,14 +1,17 @@
 from out_matrix import *
 
 m = int(input())
-result = [[0] * m] * m
+result = [[0] * m for _ in range(m)]
 
 matrix1 = [list(map(int, input().split())) for _ in range(m)]
 matrix2 = [list(map(int, input().split())) for _ in range(m)]
 
 print_matrix(result, m)
+print()
 print_matrix(matrix1, m)
+print()
 print_matrix(matrix2, m)
+print()
 
 for i in range(m):
     for j in range(m):
---
+++ temp.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+x_list = [1, 5, -3]
+
+y_list = [-5, 6, 1]
+
+plt.plot(x_list, x_list)
+
+plt.show()
