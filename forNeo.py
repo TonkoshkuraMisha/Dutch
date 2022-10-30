85d9b1c5e990ea350bb6d538c38d64805333b54a
tonkoshkura.misha <tonkoshkura.misha@gmail.com>
2022-10-30 21:41:00 +0200

    some modification of Neo

--- forNeo.py
+++ forNeo.py
@@ -1,14 +1,20 @@
 import random
+import time
 from colorama import Fore
 
+#
+# def get_random_string():
+#     out_list = [chr(j) for j in range(500, 10500)]
+#     random.shuffle(out_list)
+#     # symbol = random.choice(out_list)
+#     out_string = ''.join(out_list)
+#     return out_string
+#
+#
+# for i in range(1, 10_000):
+#     print(Fore.GREEN + get_random_string())
 
-def get_random_string():
-    out_list = [chr(j) for j in range(500, 10500)]
-    random.shuffle(out_list)
-    # symbol = random.choice(out_list)
-    out_string = ''.join(out_list)
-    return out_string
+for j in range(1, 100_001):
+    print(Fore.GREEN + f"{j}^{j} = {pow(j, j)}")
+    time.sleep(1)
 
-
-for i in range(1, 10_000):
-    print(Fore.GREEN + get_random_string())
